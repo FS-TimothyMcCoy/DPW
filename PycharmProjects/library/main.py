e1f5ad5cfcf4025b4a25f938324084b487db7fd0
FS-TimothyMcCoy <tjfatterstacks@live.com>
2016-01-12 13:52:59 -0500

    made raw inputs
    made input prompts

--- PycharmProjects/library/main.py
+++ PycharmProjects/library/main.py
@@ -1,2 +1,5 @@
-class Student():
-    def __init__(self, n, y ):
+class Person():
+    def __init__(self):
+        self.name = raw_input("What is your name?")
+        self.birth_year = raw_input("What year were you born?")
+        self.current_year = raw_input("What is the current year?")
