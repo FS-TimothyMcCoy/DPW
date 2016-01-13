08946dc31c7291e00b46d6a26ccf20fefacfaa9c
FS-TimothyMcCoy <tjfatterstacks@live.com>
2016-01-12 23:43:49 -0500

    changed
    import and library class fixed

---
+++ PycharmProjects/library/lib.py
@@ -0,0 +1,25 @@
+class Library():
+    def __init__(self):
+        pass
+    def get_age(self, cy, by):
+        return cy - by
+    def get_color(self, c):
+        return c
+    def get_haircolor(self, hc):
+        return hc
+
+
+class Printer():
+    def __init__(self):
+        pass
+    def print_out(self, a):
+        print "You are " +str(a)+ " years old."
+    def print_color(self, c):
+        print "Your favorite color is " +c
+    def print_haircolor(self, hc):
+        print "Your hair color is " +hc
+
+
+
+
+
--- PycharmProjects/library/main.py
+++ PycharmProjects/library/main.py
@@ -1,5 +1,22 @@
-class Person():
+from lib import Library, Printer
+
+
+class Main_Handler():
     def __init__(self):
-        self.name = raw_input("What is your name?")
-        self.birth_year = raw_input("What year were you born?")
-        self.current_year = raw_input("What is the current year?")
+        l = Library()
+        p = Printer()
+        name = raw_input("What is your name?")
+        fav_color = raw_input("What is your favorite color?")
+        hair_color = raw_input("What is your hair color?")
+        birth_year = int(raw_input("What year were you born?(4 digits)"))
+        current_year = int(raw_input("What is the current year?(4 digits)"))
+        age = l.get_age(current_year, birth_year)
+        p.print_out(age)
+        color = l.get_color(fav_color)
+        p.print_color(color)
+        haircolor = l.get_haircolor(hair_color)
+        p.print_haircolor(haircolor)
+
+
+
+main = Main_Handler()
